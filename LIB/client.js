4a6dcf2be723dac397811694a90be42955f276be
Ober Rémi <oberremi@gmail.com>
2023-05-23 14:29:10 +0200

    final push

---
+++ LIB/client.js
@@ -0,0 +1,19 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url'
+
+import { createClient } from '@sanity/client'
+
+export const client = createClient({
+    projectId: '7ky3k5bq',
+    dataset: 'production',
+    apiVersion: "2022-03-10",
+    useCdn: true,
+    token: process.env.SANITY_TOKEN,
+})
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
+
+
+
---
+++ components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import Link from 'next/link'
+import Image from 'next/image';
+import { AiOutlineShopping } from 'react-icons/ai' 
+
+import { Cart } from "./"
+import { useStateContext } from '@/context/StateContext'
+
+function Navbar() {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>
+          <Image
+            src="/../public/logo_sound.png"
+            width={200}
+            height={100}
+            alt="Picture of the author"
+          />
+        </Link>
+        
+      </p>
+
+      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>{totalQuantities}</span>
+      </button>
+
+      {showCart && <Cart />}
+    </div>
+  )
+}
+
+export default Navbar
